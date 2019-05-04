6d92470ab0986ca331e259cbd519795c92080fb9
targinteanu <targinteanu@outlook.com>
2019-05-03 20:19:17 -0400

    before adding the second step

--- twostep.m
+++ twostep.m
@@ -7,7 +7,7 @@
 
 KNNfunc = @(Xtrain, Ytrain, Xtest) knnclassify(Xtest, Xtrain, Ytrain, 20);
 
-MLfunc = @(xtr,ytr,xte) SVMfunc(xtr,ytr,xte);
+MLfunc = @(xtr,ytr,xte) logregfunc(xtr,ytr,xte);
 
 sub = 1;
 Y = train_dg{sub};
@@ -27,12 +27,9 @@ Y = Y(trim:end,:);
 % normalize X
 %X = X - mean(X, 2); X = X./std(X, [], 2);
 %Y = Y - mean(Y, 2); Y = Y./std(Y, [], 2);
-%{
-X = X - mean(X); 
-Ctrim = C(:,1:50);
-X = X*Ctrim;
-%}
-X = tsne(X);
+%X = X - mean(X); 
+%Ctrim = C(:,1:50);
+%X = X*Ctrim;
 
 %{
 sub = 1;
@@ -47,7 +44,7 @@ X0 = X; X = S(:,1:100);
 
 addpath(genpath('C:\Users\Toren\Desktop\BE521final\libsvmmatlab'))
 savepath
-
+%{
 tic 
 crossvalrho = arrayfun(@(f) mean(...
     crossval(@(Xtr,Ytr,Xte,Yte) ...
@@ -58,26 +55,29 @@ toc
 
 nTR = floor(length(Y(:,1))*.5);
 YTR = Y(1:nTR,:); YTE = Y((nTR+1):end,:);
-XTR = X(1:nTR,:); XTE = X((nTR+1):end,:);
 Ytrainpred = zeros(size(YTR)); Ytestpred = zeros(size(YTE));
 figure; clear ax;
 for f = 1:5
-    %[Ytrainpred(:,f), trainprob] = MLfunc(XTR, YTR(:,f), XTR);
-    %[Ytestpred(:,f), testprob] = MLfunc(XTR, YTR(:,f), XTE);
-    Ytrainpred(:,f) = MLfunc(XTR, YTR(:,f), XTR);
-    Ytestpred(:,f) = MLfunc(XTR, YTR(:,f), XTE);
+    Ctrim = C{f}(:,1:30);
+    Xpc = X*Ctrim;
+    XTR = Xpc(1:nTR,:); XTE = Xpc((nTR+1):end,:);
+    
+    [Ytrainpred(:,f), trainprob] = MLfunc(XTR, YTR(:,f), XTR);
+    [Ytestpred(:,f), testprob] = MLfunc(XTR, YTR(:,f), XTE);
+    %Ytrainpred(:,f) = MLfunc(XTR, YTR(:,f), XTR);
+    %Ytestpred(:,f) = MLfunc(XTR, YTR(:,f), XTE);
     
     ax(f) = subplot(5,1,f);
     plot(Y(:,f), 'k'); hold on; 
     plot([Ytrainpred(:,f); Ytestpred(:,f)], '.b');
-    %plot([trainprob; testprob], '--b');
+    plot([trainprob; testprob], '--b');
     plot([nTR nTR], [min(Y(:)) max(Y(:))], 'r');
     grid on; 
     testacc = sum(Ytestpred(:,f) == YTE(:,f))/length(YTE(:,f));
     trainacc = sum(Ytrainpred(:,f) == YTR(:,f))/length(YTR(:,f));
     title(['test acc. = ' num2str(testacc) ...
-        ' | train ' num2str(trainacc) ...
-        ' | crossval ' num2str(crossvalrho(f))]);
+        ' | train ' num2str(trainacc)]);% ...
+%        ' | crossval ' num2str(crossvalrho(f))]);
     ylim([.5 2.5]);
 end
 linkaxes(ax); clear ax;

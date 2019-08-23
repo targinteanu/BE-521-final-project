c81aa0e6e450fa411b2d0d381085ebbdeb68993d
targinteanu <targinteanu@outlook.com>
2019-08-23 13:27:45 -0400

    fixed error in calculating derivative analytically

--- gradientDecode.m
+++ gradientDecode.m
@@ -4,17 +4,16 @@
 
 % test: artificial Y and X -----------------------------------------
 %Ycoeffs = [20, 0, 10, -.01, -40]; Ypowers = [1,1,1,1,1];
-Ycoeffs = [-.085, .00062]; Ypowers = [2, 4];
+%Ycoeffs = [-.085, .00062]; Ypowers = [2, 4];
 %Ycoeffs = [5, 5]; Ypowers = [2, 2];
-%Ycoeffs = [20 -10 .01 -3 -15 .01]; Ypowers = [3, 3, 3, 3, 3];
+Ycoeffs = [20 -10 .01 -3 -15 .01]; Ypowers = [1, 3, 5, 3, 1, 1];
+%Ycoeffs = [3, 3]; Ypowers = [2, 2];
 numvars = length(Ycoeffs);
 x0 = 1:1000; X = zeros(1000,numvars);
 for var = 1:numvars
     X(:,var) = rand*sin(rand*x0 + rand) + 10*rand*sin(rand*x0 + rand) ...
         + rand*sin(10*rand*x0 + rand) + 10*rand*sin(10*rand*x0 + rand);
 end
-%Y = Ycoeffs(1)*X(:,1) + Ycoeffs(2)*X(:,2) + Ycoeffs(3)*X(:,3) ...
-%    + Ycoeffs(4)*X(:,4) + Ycoeffs(5)*X(:,5);
 Y = zeros(length(X),1); 
 for var = 1:numvars
     Y = Y + Ycoeffs(var)*X(:,var).^Ypowers(var);
@@ -29,15 +28,19 @@ SD = std(X);
 
 IdxSort = zeros(size(X)); IdxUnsort = zeros(size(X));
 Xsorted = zeros(size(X)); Ysorted = zeros(size(X));
+dXsort = zeros(size(Xsorted)); dYsort = zeros(size(Ysorted));
 
 for var = 1:n
     [Xsorted(:,var), IdxSort(:,var)] = sort(X(:,var));
     Ysorted(:,var) = Y(IdxSort(:,var));
     [~, IdxUnsort(:,var)] = sort(IdxSort(:,var));
+    
+    dXsort(:,var) = gradient(Xsorted(:,var)); 
+    dYsort(:,var) = gradient(Ysorted(:,var));
 end
 
-dXsort = Xsorted(2:end,:) - Xsorted(1:(end-1),:); dXsort = [dXsort(1,:); dXsort];
-dYsort = Ysorted(2:end,:) - Ysorted(1:(end-1),:); dYsort = [dYsort(1,:); dYsort];
+%dXsort = Xsorted(2:end,:) - Xsorted(1:(end-1),:); dXsort = [dXsort(1,:); dXsort];
+%dYsort = Ysorted(2:end,:) - Ysorted(1:(end-1),:); dYsort = [dYsort(1,:); dYsort];
 
 dYdXsort = dYsort./dXsort; 
 dYdXsort(isinf(dYdXsort)) = 0;
@@ -47,24 +50,22 @@ for var = 1:n
     dYdX(:,var) = dYdXsort(IdxUnsort(:,var),var);
 end
 
-% test: 2D problem ----------------------------------------
-%figure; plot3(X(:,1), X(:,2), Y, '.'); hold on; grid on; 
-figure; 
-quiver(X(:,1), dYdX(:,1), X(:,2), dYdX(:,2));
-% end test ------------------------------------------------
-
 % test: overwrite dYdX ------------------------------------
 for var = 1:n
-    dYdX(:,var) = Ycoeffs(var)*X(:,var).^(Ypowers(var)-1);
+    dYdX(:,var) = Ycoeffs(var)*Ypowers(var)*X(:,var).^(Ypowers(var)-1);
 end
+
+%{
+figure; hold on; grid on; 
+colr = 'rbmgcy';
+for var = 1:n
+    plot(X(:,var), dYdX(:,var), ['.' colr(var)]); 
+    plot(X(:,var), dYdX_act(:,var), ['o' colr(var)]); 
+end
+%}
 % end test ------------------------------------------------
 
-% test: 2D problem ----------------------------------------
-figure; plot3(X(:,1), X(:,2), Y, '.'); hold on; grid on; 
-%figure; 
-quiver(X(:,1), dYdX(:,1), X(:,2), dYdX(:,2));
-% end test ------------------------------------------------
-
+%%{
 wstart = (SD/norm(SD))';
 %stepsize = 1e-18;
 stepsize = 1e-5;
@@ -84,14 +85,14 @@ wangles = acos(wangles);
 figure; 
 subplot(2,1,1); plot(gwmags); ylabel('grad mag'); xlabel('steps'); grid on;
 subplot(2,1,2); plot(wangles); ylabel('w direction change'); xlabel('steps'); grid on;
-%}
+%%}
 
 %%{
 figure; 
 subplot(2,1,1); plot(X*w, 'LineWidth', 2); grid on; ylabel('X'); xlabel('time');  
 hold on; plot(X);
 subplot(2,1,2); plot(Y); grid on; ylabel('Y'); xlabel('time');
-%}
+%%}
 
 figure; plot(X*w, Y, '*'); grid on; xlabel('X'); ylabel('Y'); 
 hold on; 
@@ -106,6 +107,7 @@ hold on;
 for var = 1:n
     plot(X(:,var), Y, '.');
 end
+title('first PC')
 % ----------------------------------------------------------
 
 function grad = getgrad(w, dydx)
@@ -115,3 +117,5 @@ integrd = dotp.*dydx;
 grad = (2*sum(integrd))';
 
 end
+
+%}
